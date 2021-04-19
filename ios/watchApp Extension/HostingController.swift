c45e79532ab093f1909f0d53743ac07791f7adc1
Jonathan Bones <bonesyblue@gmail.com>
2021-04-19 20:11:09 +0200

    feat: Add watchOS target to Xcode project

---
+++ ios/watchApp Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  watchApp Extension
+//
+//  Created by Jonathan Bones on 19.04.21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ ios/watchApp Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  watchApp Extension
+//
+//  Created by Jonathan Bones on 19.04.21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
